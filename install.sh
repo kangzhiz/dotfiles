30986a1bfafdffed2c4df04130f8ef29d4bf0de4
John Zhao <kangzhiz@pm.me>
2022-03-16 01:52:23 -0400

    Update scripts to be usable from any directory

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
 
+DOTFILES_DIR="$HOME/Documents/dotfiles"
+
 # check if repo was cloned into the correct directory
-if [ $PWD != "$HOME/Documents/dotfiles" ]; then
+if [ $PWD != $DOTFILES_DIR ]; then
     printf "Oops, wrong directory! Clone into ~/Documents/dotfiles.\n"
     exit
 fi
 
 # install homebrew packages
-if [ -f scripts/brew-install-all ]; then
-    source scripts/brew-install-all
+if [ -f $DOTFILES_DIR/scripts/brew-install-all ]; then
+    source $DOTFILES_DIR/scripts/brew-install-all
 else
     printf "Error! \"scripts/brew-install-all\" is missing.\n"
     exit
 fi
 
 # install dotfiles
-if [ -f scripts/stow-dotfiles ]; then
-    source scripts/stow-dotfiles
+if [ -f $DOTFILES_DIR/scripts/stow-dotfiles ]; then
+    source $DOTFILES_DIR/scripts/stow-dotfiles
 else
     printf "Error! \"scripts/stow-dotfiles\" is missing.\n"
     exit
